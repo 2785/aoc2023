c3d69d06b93c409982bb544bf03d8a163affc04b
Shiqi Zhao <zhao.shiqi.art@gmail.com>
2023-12-06 00:29:48 -0500

    feat: add a timer too cus why not

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 )
@@ -18,8 +20,16 @@ func init() {
 	s = l.Sugar()
 }
 
+var start time.Time
+
 var root = &cobra.Command{
 	Use: "aoc2023",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		start = time.Now()
+	},
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		s.Infof("took %s", time.Since(start))
+	},
 }
 
 func c(e error) {
